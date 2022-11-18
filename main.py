a80ecb8520c4cc1f025cb03db4608bec17b730e9
Ilya <laminariakua@gmail.com>
2022-11-18 21:23:57 +0300

    methods

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ class Order2line:
                 inputFactors[4] /= 2
                 self.factors = inputFactors
         self.i1 = inputFactors[0] + inputFactors[2]
+    def i1_update():
+        self.i1 = self.factors[0] + self.factors[2]
+    def i2_update():
+        self.i2 = self.factors[0] * self.factors[2] - self.factors[1] * self.factors[1]
+    def i3_update():
+        self.i3 = self.factors[0] * self.factors[2] - self.factors[1] * self.factors[1]
