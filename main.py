72144149c45569b026090efdbec14c5bd1a05c5e
Ilya <laminariakua@gmail.com>
2022-11-19 01:52:14 +0300

    vbeta1.0

--- main.py
+++ main.py
@@ -24,6 +24,11 @@ class Order2line:
         self.i3_update()
         if self.i2 == 0:
             self.type = 'parabola'
+        elif self.i2 > 0:
+            self.type = 'ellipse'
+        else:
+            self.type = 'hyperbola'
+        if self.type == 'parabola':
             if self.factors[1] == 0:
                 self.phi2 = 0
                 self.phi = 0
@@ -36,14 +41,28 @@ class Order2line:
                 if self.factors[1] * math.sin(self.phi2) + (self.factors[0] - self.factors[2]) * math.cos(self.phi2) / 2 + (self.factors[0] + self.factors[2]) / 2 != 0:
                     self.phi2 += math.pi
                 self.phi = self.phi2 / 2
-                self.factors1 = [self.factors[1] * math.sin(self.phi2) + (self.factors[0] - self.factors[2]) * math.cos(self.phi2) / 2 + (self.factors[0] + self.factors[2]) / 2, 0, - self.factors[1] * math.sin(self.phi2) - (self.factors[0] - self.factors[2]) * math.cos(self.phi2) / 2 + (self.factors[0] + self.factors[2]) / 2, self.factors[3] * math.cos(self.phi) + self.factors[4] * math.sin(self.phi), self.factors[4] * math.cos(self.phi) - self.factors[3] * math.sin(self.phi), self.factors[5]]
+                self.factors1 = self.rotate(self.phi, self.phi2, self.factors)
             self.factors2 = [0, 0, self.i1, self.factors1[3], 0, self.factors[5] - self.factors1[4]**2 / self.i1]
+        else:
+            self.factors1 = [self.factors[0], self.factors[1], self.factors[2], 0, 0, self.i3 / self.i2]
+            if self.factors[1] == 0:
+                self.factors2 = self.factors1
+            else:
+                if self.factors1[0] == self.factors1[2]:
+                    self.phi2 = math.pi / 2
+                else:
+                    self.phi2 = math.atan(2*self.factors1[1]/(self.factors1[0]-self.factors1[2]))
+                self.phi = self.phi2 / 2
+                self.factors2 = self.rotate(self.phi, self.phi2, self.factors1)
 
+    def rotate(self, phi, phi2, fac):
+        return [fac[1] * math.sin(phi2) + (fac[0] - fac[2]) * math.cos(phi2) / 2 + (fac[0] + fac[2]) / 2, 0, - fac[1] * math.sin(phi2) - (fac[0] - fac[2]) * math.cos(phi2) / 2 + (fac[0] + fac[2]) / 2, fac[3] * math.cos(phi) + fac[4] * math.sin(phi), fac[4] * math.cos(phi) - fac[3] * math.sin(phi), fac[5]]
     def inf_message(self):
         self.intr(self.factors)
         print('I1 = ', self.i1, 'I2 = ', self.i2, 'I3 = ', self.i3)
         print('type = ', self.type)
-        print()
+        self.intr(self.factors1)
+        self.intr(self.factors2)
     def intr(self, fac):
         variables = ['x^2', 'x*y', 'y^2', 'x', 'y', '']
         for i in range(6):
@@ -56,6 +75,5 @@ class Order2line:
 
 
 
-a = Order2line([9, -24, 16, -20, 110, -50])
-print(a.factors2)
-a.intr(a.factors2)
+a = Order2line([3, 10, 3, -2, -14, -13])
+a.inf_message()
